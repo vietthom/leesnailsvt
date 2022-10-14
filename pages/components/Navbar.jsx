1fe7327d6d102a221e1a32abbcd4fc208ff5dfac
vietthom <thomle0418@gmail.com>
2022-10-14 08:40:14 -0600

    Stashing

---
+++ pages/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-blue-300 text-red-400'>Navbar</div>
+  )
+}
+
+export default Navbar
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-
+import Navbar from "./components/Navbar"
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1 className='text-color'>Hello World!</h1>
     </div>
   )
