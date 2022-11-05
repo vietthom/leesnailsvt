c464649cbb0486183c45e8b0ffe9cb04eb4e992a
vietthom <thomle0418@gmail.com>
2022-11-05 13:27:41 -0600

    Start on Hero component.

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
+import HeroImg from '../public/assets/hero.jpg'
 
 const Hero = () => {
   return (
-    <div>
-      <div className='bg-red-500 w-full h-full'>
-        <h1>Hello world</h1>
-      </div>
+    <div className='bg-[#e09132] w-full h-screen py-16 px-4'>
+        <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
+            <div>
+                <h1 className='md:text-xl sm:text-3xl text-2xl font-bold pb-2'>Welcome to Lee's Nails</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, alias!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi repellat consequuntur, laboriosam consectetur quas cum ducimus delectus porro praesentium dolor, rem debitis ea maxime sunt saepe. Magnam sint harum minus?</p>
+                <button className='hidden md:block border-2 border-black p-2 ' type='button'>
+                    <a href='https://booking.gocheckin.net/v2/12536' rel='noreferrer' target='_blank'>Book Now</a>
+                </button>
+            </div>
+            <Image src={HeroImg} alt='/'/>
+        </div>
     </div>
   )
 }
