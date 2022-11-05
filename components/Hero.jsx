9057686c1d862715a9f55525bd8804a56453b541
vietthom <thomle0418@gmail.com>
2022-11-05 13:36:46 -0600

    Edit Hero component to allow Vercel to compile build.

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <div className='bg-[#e09132] w-full h-screen py-16 px-4'>
         <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
             <div>
-                <h1 className='md:text-xl sm:text-3xl text-2xl font-bold pb-2'>Welcome to Lee's Nails</h1>
+                <h1 className='md:text-xl sm:text-3xl text-2xl font-bold pb-2'>Welcome to Lee Nails</h1>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, alias!</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi repellat consequuntur, laboriosam consectetur quas cum ducimus delectus porro praesentium dolor, rem debitis ea maxime sunt saepe. Magnam sint harum minus?</p>
                 <button className='hidden md:block border-2 border-black p-2 ' type='button'>
